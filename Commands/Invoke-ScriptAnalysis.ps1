ff04f23d4ad050c517c9016dc445bf11666e43d9
Andy Svintsitsky <andy.svintsitsky@gmail.com>
2026-04-16 15:17:33 +0300

    Update Invoke-ScriptAnalysis.ps1

--- Commands/Invoke-ScriptAnalysis.ps1
+++ Commands/Invoke-ScriptAnalysis.ps1
@@ -1,1 +1,26 @@
 
+function Invoke-ScriptAnalysis
+{
+    [CmdletBinding(SupportsShouldProcess=$true, 
+                  ConfirmImpact='Medium')]
+    [Alias()]
+    [OutputType([String])]
+    Param
+    (
+       $Text
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        if ($pscmdlet.ShouldProcess("script $($Text | select-object -First 5)"))
+        {
+          #TO DO
+        }
+    }
+    End
+    {
+    }
+}
