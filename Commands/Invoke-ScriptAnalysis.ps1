7192442a8c245dd5c85df263362297a4a48411ed
Andy Svintsitsky <andy.svintsitsky@gmail.com>
2026-04-21 12:06:19 +0300

    Connect to Azure and fetch API key from Key Vault
    Added functionality to connect to Azure and retrieve API key from Key Vault.

--- Commands/Invoke-ScriptAnalysis.ps1
+++ Commands/Invoke-ScriptAnalysis.ps1
@@ -17,6 +17,10 @@ function Invoke-ScriptAnalysis
     {
         if ($pscmdlet.ShouldProcess("script $($Text | select-object -First 5)"))
         {
+            Import-Module Az.Accounts 
+            connect-AzAccount -Subscription "094200e5-c0b6-4890-aeec-5a21a93a690e" -Identity | Out-Null
+            $KeyVaultName="NoShellGames-KV"
+            $apiKey=Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name "NoShellGamesAPIKey" -AsPlainText
           #TO DO
         }
     }
