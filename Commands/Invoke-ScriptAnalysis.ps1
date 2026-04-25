6cc843afdd73f780cfcfa91175e463c59d208625
Andy Svintsitsky <andy.svintsitsky@gmail.com>
2026-04-25 06:53:59 -0400

    Fix API URL formatting in Invoke-ScriptAnalysis.ps1

--- Commands/Invoke-ScriptAnalysis.ps1
+++ Commands/Invoke-ScriptAnalysis.ps1
@@ -21,7 +21,7 @@ function Invoke-ScriptAnalysis
             connect-AzAccount -Subscription "094200e5-c0b6-4890-aeec-5a21a93a690e" -Identity | Out-Null
             $KeyVaultName="NoShellGames-KV"
             $apiKey=Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name "NoShellGamesAPIKey" -AsPlainText
-            $apiUrl = "https://noshellgames.azure-api.net/noshellgames/AnalyzeScript?subscription-key=$apiKey
+            $apiUrl = "https://noshellgames.azure-api.net/noshellgames/AnalyzeScript?subscription-key=$apiKey"
             $apiResponse = Invoke-RestMethod -Uri $apiUrl
             $HTMLTemplate=Get-Content $(Join-Path -Path "/usr/local/share/powershell/Modules/NoShellGames-Web/Web" -ChildPath "index.html") -Raw
             $HTMLResults=[System.Text.StringBuilder]::new()
