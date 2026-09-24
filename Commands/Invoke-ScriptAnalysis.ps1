2563a8726298c6cbe38cf69eefa5be392fd7ed03
Andy Svintsitsky <andy.svintsitsky@gmail.com>
2026-09-24 11:11:44 -0400

    Update Invoke-ScriptAnalysis.ps1

--- Commands/Invoke-ScriptAnalysis.ps1
+++ Commands/Invoke-ScriptAnalysis.ps1
@@ -60,7 +60,7 @@ function Invoke-ScriptAnalysis
                                         @{ Name = 'Behaviors'; Value = $apiResponse.assessment.behaviorCount },
                                         @{ Name = 'Capabilities'; Value = $apiResponse.capabilityCount }
                                     )){
-                          $AssessmentStats+=$("<div class=`"result-stat`"><strong>$($s.Value)</strong><span>$($s.Name)</span></div>"
+                          $AssessmentStats+=$("<div class=`"result-stat`"><strong>$($s.Value)</strong><span>$($s.Name)</span></div>")
                     }
                 $AssessmentStats+="</div>"
                 }
